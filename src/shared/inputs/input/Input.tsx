9e5b1e686665fdaba5c9a353fa19c3ac40a8338f
Lacerda Junior <jralima01@gmail.com>
2024-04-01 18:53:27 -0300

    feat: create first global component

--- src/shared/inputs/input/Input.tsx
+++ src/shared/inputs/input/Input.tsx
@@ -3,16 +3,16 @@ import { Input as InputAntd, InputProps as InputPropsAntd } from 'antd';
 import { BoxInput, TitleInput } from './Input.styles';
 
 interface InputProps extends InputPropsAntd {
-    title?: string;
+  title?: string;
 }
 
 const Input = ({ title, ...props }): InputProps => {
-    return (
-        <BoxInput>
-            {title && <TitleInput>{title}</TitleInput>}
-            <InputAntd {...props} />
-        </BoxInput>
-    );
+  return (
+    <BoxInput>
+      {title && <TitleInput>{title}</TitleInput>}
+      <InputAntd {...props} />
+    </BoxInput>
+  );
 };
 
 export default Input;
